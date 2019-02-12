2f969c110f86b955196c7ef870b4a81a6eb44b1e
Gonzalo Escandarani <gonziesc@gmail.com>
2019-02-12 10:56:32 -0300

    adding nodejs config

---
+++ index.js
@@ -0,0 +1,13 @@
+const eslintConfig = require('eslint-config-wolox');
+module.exports = {...eslintConfig, 
+  rules: {
+    "arrow-parens": ["error", "as-needed"],
+    "arrow-spacing": ['error', { before: true, after: true }],
+    "camelcase": 0,
+    "max-len": ["error", { "code": 120 }],
+    "max-nested-callbacks": 0,
+    "max-params": ["error", 4],
+    "no-magic-numbers": 0,
+  }
+};
+
