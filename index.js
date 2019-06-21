57bd1a337d24c3eeb49808fdeb7944e46570cd7d
raparicio6 <rodrigoaparicio6@gmail.com>
2019-06-21 20:06:23 -0300

    adding the other rules

--- index.js
+++ index.js
@@ -16,22 +16,29 @@ module.exports = {
     sourceType: "module"
   },
   extends: ["wolox"],
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/
   plugins: ["import"],
   globals: {
     __DEV__: true
   },
   rules: {
+    "arrow-body-style": ["error", "as-needed"],
     "arrow-parens": ["error", "as-needed"],
     "arrow-spacing": ["error", { before: true, after: true }],
     "camelcase": 0,
+    "curly": ["error", "multi"],
+    "global-require": "error",
+    "import/order": ["error", { groups: [["builtin", "external", "internal"]] }],
+    "import/named": "error",
+    "import/no-duplicates": "error",
+    "import/newline-after-import": "error",
+    "import/no-cycle": ["error", { maxDepth: Infinity }],
+    "import/no-useless-path-segments": "error",
     "max-len": ["error", { code: 100 }],
     "max-nested-callbacks": 0,
     "max-params": ["error", 4],
     "no-magic-numbers": 0,
-    "curly": ["error", "multi"],
-    "global-require": "error",
-    "one-var": ["error", "never"],
-    "import/order": ["error", { groups: [["builtin", "external", "internal"]] }],
+    "one-var": ["error", "never"]
   },
   settings: {
     "import/resolver": {
