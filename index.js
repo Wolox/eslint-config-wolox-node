17b33c85a174536d52b5e6a02867cf9a1cc4b89c
raparicio6 <rodrigoaparicio6@gmail.com>
2019-06-21 17:39:16 -0300

    adding some rules

--- index.js
+++ index.js
@@ -16,17 +16,22 @@ module.exports = {
     sourceType: "module"
   },
   extends: ["wolox"],
+  plugins: ["import"],
   globals: {
     __DEV__: true
   },
   rules: {
     "arrow-parens": ["error", "as-needed"],
-    "arrow-spacing": ['error', { before: true, after: true }],
+    "arrow-spacing": ["error", { before: true, after: true }],
     "camelcase": 0,
-    "max-len": ["error", { "code": 120 }],
+    "max-len": ["error", { code: 100 }],
     "max-nested-callbacks": 0,
     "max-params": ["error", 4],
     "no-magic-numbers": 0,
+    "curly": ["error", "multi"],
+    "global-require": "error",
+    "one-var": ["error", "never"],
+    "import/order": ["error", { groups: [["builtin", "external", "internal"]] }],
   },
   settings: {
     "import/resolver": {
